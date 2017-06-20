41f8409ddb664e18b4bf6cf17a11936427037c27
Charly Sparks <charlotte.spark@gmail.com>
2017-06-20 02:19:34 -0400

    Study Drill
    Prompt 4 for exeercise 3 was to rewrite this file so that the numbers
    were more accurate by making them floats.

--- ex3.py
+++ ex3.py
@@ -1,7 +1,7 @@
 print("I will now count my chickens:")
 
 print("Hens", 25 + 30 / 6)
-print("Roosters", 100 - 25 * 3 % 4)
+print("Roosters", 100 - 25 * 3 % 4 / 1)
 
 print("Now I will count my eggs:")
 
@@ -11,8 +11,8 @@ print("Is it true that 3 + 2 < 5 - 7?")
 
 print(3 + 2 < 5 - 7)
 
-print("What is 3 + 2?", 3 + 2)
-print("What is 5 - 7?", 5 - 7)
+print("What is 3 + 2?", 3 + 2 / 1)
+print("What is 5 - 7?", 5 - 7 / 1)
 
 print("Oh, that's why it's False.")
 
