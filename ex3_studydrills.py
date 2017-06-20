1c249fbf7b0967250033421e0a220a846ad4a5f0
Charly Sparks <charlotte.spark@gmail.com>
2017-06-20 02:26:03 -0400

    ex3 Study Drills
    Per the instructions, I made a duplicate file that has comments over
    each line of code. I also added two new equations to the bottom.

---
+++ ex3_studydrills.py
@@ -0,0 +1,36 @@
+# This line is will print the sentence as a string about my chickens.
+print("I will now count my chickens:")
+
+# Print number of Hens
+print("Hens", 25 + 30 / 6)
+# Print number of Roosters
+print("Roosters", 100 - 25 * 3 % 4)
+
+# This line will print the sentence as a string about my eggs
+print("Now I will count my eggs:")
+# Print number of eggs '/' will print a float value
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# Print math statement as a string
+print("Is it true that 3 + 2 < 5 - 7?")
+# Will compute the expression as a Boolean expression
+print(3 + 2 < 5 - 7)
+
+# Calculate 3+2 and print the result
+print("What is 3 + 2?", 3 + 2)
+# Calculate 5-7 and print the result
+print("What is 5 - 7?", 5 - 7)
+
+# Print string
+print("Oh, that's why it's False.")
+#Print string
+print("How about some more.")
+
+#Calculates each equation and gives the answer as a Boolean expression
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+# Other things to calculate
+print(3 % 2)
+print(3 / 2)
